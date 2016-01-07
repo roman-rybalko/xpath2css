6d2802083a7fcac23559a6b85101004d026b974a
Roman Rybalko <devel@romanr.info>
2016-01-07 21:41:09 +0300

    contains, starts-with, ends-with, and, spaces

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 /**
+ *
  * The simplest XPATH to CSS selector conversion tool.
  *
  * https://github.com/roman-rybalko/xpath2css
@@ -8,14 +9,29 @@
 'use strict';
 
 function xpath2css(xpath) {
+	if (xpath.match(/\s+or\s+/))
+		throw 'xpath2css: "or" xpath clause is not supported';
 	return xpath
-		.replace(/\[(\d+?)\]/g, function(s,m1){ return '['+(m1-1)+']'; })
-		.replace(/^\/+/, '')
-		.replace(/\/{2}/g, ' ')
-		.replace(/\/+/g, ' > ')
-		.replace(/@/g, '')
-		.replace(/\[(\d+)\]/g, ':eq($1)')
-		.replace(/^\s+/, '');
+		.replace(/\s+and\s+/g, '][')  // "and" clause
+		.replace(/^\s+/, '')  // extra space
+		.replace(/\s+$/, '')  // extra space
+		.replace(/\s*\[\s*/g, '[')  // extra space
+		.replace(/\s*\]\s*/g, ']')  // extra space
+		.replace(/\s*\(\s*/g, '(')  // extra space
+		.replace(/\s*\)\s*/g, ')')  // extra space
+		.replace(/\s*(\/+)\s*/g, '$1')  // extra space
+		.replace(/\s*,\s*/g, ',')  // extra space
+		.replace(/\s*=\s*/g, '=')  // extra space
+		.replace(/^\/+/, '')  // remove first "/" since it's irrelevant in css
+		.replace(/\/\/+/g, '//')  // fix "////" "///"
+		.replace(/\[(\d+)\]/g, function(s,m1){return ':eq('+(m1-1)+')';})  // "//*[3]"
+		.replace(/\/\//g, ' ')  // descendant
+		.replace(/\//g, ' > ')  // child
+		.replace(/@/g, '')  // attribute
+		.replace(/contains\((\S+),(\S+?|"[^"]+?"|'[^']+?')\)/g, '$1*=$2')  // "contains" clause
+		.replace(/starts\-with\((\S+),(\S+?|"[^"]+?"|'[^']+?')\)/g, '$1^=$2')  // "starts-with" clause
+		.replace(/ends\-with\((\S+),(\S+?|"[^"]+?"|'[^']+?')\)/g, '$1\$=$2')  // "ends-with" clause
+	;
 }
 
 module.exports = xpath2css;
--- test.js
+++ test.js
@@ -6,19 +6,31 @@ test('convert', function(assert) {
 
   actual = xpath2css('/HTML/HEAD/TITLE');
   expected = 'HTML > HEAD > TITLE';
-  assert.equal(actual, expected, 'should handle upper case');
+  assert.equal(actual, expected, 'child, upper case');
 
   actual = xpath2css('/html/head/title');
   expected = 'html > head > title';
-  assert.equal(actual, expected, 'should handle lower case');
+  assert.equal(actual, expected, 'child, lower case');
 
-  actual = xpath2css('/HTML/BODY/DIV[@id=\'menu\']/NAV/UL[5]');
-  expected = 'HTML > BODY > DIV#menu > NAV > UL:nth-of-type(5)';
-  assert.equal(actual, expected);
+  actual = xpath2css("/HTML/BODY/DIV[@id='menu']/NAV/UL[5]");
+  expected = "HTML > BODY > DIV[id='menu'] > NAV > UL:eq(4)";
+  assert.equal(actual, expected, 'attribute, index');
 
-  actual = xpath2css('//div[@id="foo"][2]/span[@class="bar"]//a[contains(@class, "baz")]//img[1]');
-  expected = 'div#foo:nth-of-type(2) > span.bar a[class*=baz] img:first-of-type';
-  assert.equal(actual, expected);
+  actual = xpath2css('//div[contains(@id, foo )][2]/ span [ contains ( @class , \' bar\' ) ] //a[contains(@class, "baz ")]//img[1]');
+  expected = 'div[id*=foo]:eq(1) > span[class*=\' bar\'] a[class*="baz "] img:eq(0)';
+  assert.equal(actual, expected, 'descendant, "contains" clause, spaces');
+
+  actual = xpath2css('//*[starts-with(@data-id, " test ")]');
+  expected = '*[data-id^=" test "]';
+  assert.equal(actual, expected, '"starts-with" clause, spaces');
+
+  actual = xpath2css('//*[ends-with(@data-test , "test")]');
+  expected = '*[data-test$="test"]';
+  assert.equal(actual, expected, '"ends-with" clause, spaces');
+
+  actual = xpath2css('//*[ends-with(@data-test , "test") and @data-x = "xx"]');
+  expected = '*[data-test$="test"][data-x="xx"]';
+  assert.equal(actual, expected, '"and" clause');
 
   assert.end();
 });
